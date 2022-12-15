0827f505efb18c4c6e90f070a5f10e422187ad67
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-12-15 14:04:01 +0100

    feat: setup eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',    
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "semi": "error"
+  }
+}
