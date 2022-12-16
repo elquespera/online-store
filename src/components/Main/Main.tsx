deec3bd22e59f79e91533c8f36b9eeb90fe65219
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-12-16 11:38:11 +0100

    feat: add Main component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import styles from './Main.module.scss';
+
+const Main = () => {
+  return (
+    <main className={styles.main}>
+      <div>Content</div>
+    </main>
+  );
+};
+
+export default Main;
