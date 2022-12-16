925afdd03b9bc81656711463c34cc8df0e082b72
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-12-16 11:57:37 +0100

    feat: add react router

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,20 @@
 import styles from './Main.module.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const Main = () => {
   return (
     <main className={styles.main}>
-      <div>Content</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">Store Front Component</Route>
+          <Route path="product-details">
+            <Route path=":product-id">Product Details Component</Route>
+          </Route>
+          <Route path="cart">Cart Compontn</Route>
+          <Route path="*">404 Component</Route>
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 };
