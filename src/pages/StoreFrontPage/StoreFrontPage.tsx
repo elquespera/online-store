4f0b383f0192ed4ca3706d419890fcbfcf66d0a1
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-12-23 10:07:30 +0100

    feat: implement front page layout

--- src/pages/StoreFrontPage/StoreFrontPage.tsx
+++ src/pages/StoreFrontPage/StoreFrontPage.tsx
@@ -2,43 +2,52 @@ import { useEffect, useState } from 'react';
 import OneProduct from '../../components/OneProduct/OneProduct';
 import { Product } from '../../http/interface';
 import { getAllProducts } from '../../http/product';
+import Card from '../../components/Card/Card';
 import SelectInput from '../../components/SelectInput/SelectInput';
 import styles from './StoreFrontPage.module.scss';
 
-const StoreFrontPage = () => {
+const mockSelect = (titles: string[], max: number, length = 20) => {
   const [products, setProducts] = useState<Product[] | undefined>([]);
 
   useEffect(() => {
     getAllProducts().then((res) => setProducts(res));
   }, []);
 
-  const categories = Array.from({ length: 20 })
+  return Array.from({ length })
     .fill(0)
     .map(() => {
       return {
-        title: [
-          'groceries',
-          'laptops',
-          'smaptphones',
-          'skincare',
-          'home decorations',
-        ][Math.floor(Math.random() * 5)],
+        title: titles[Math.floor(Math.random() * titles.length)],
         checked: Math.random() < 0.5,
-        max: 5,
-        found: Math.floor(Math.random() * 5),
+        max,
+        found: Math.floor(Math.random() * max),
       };
     });
+};
+
+const StoreFrontPage = () => {
+  const categories = mockSelect(
+    ['groceries', 'laptops', 'smaptphones', 'skincare', 'home decorations'],
+    5,
+    20
+  );
+  const brands = mockSelect(['Apple', 'Samsung', 'Oppo', 'HP Pavilion'], 8, 25);
   return (
     <div className={styles['store-front-page']}>
-      <h2>Products</h2>
-      {products ? (
-        products.map((product) => (
-          <OneProduct key={product.id} product={product} />
-        ))
-      ) : (
-        <div>Products not found</div>
-      )}
-      <SelectInput title="Categories" options={categories} />
+      <Card title="Filters">
+        <div className={styles['filters-panel']}>
+          {products ? (
+            products.map((product) => (
+              <OneProduct key={product.id} product={product} />
+            ))
+          ) : (
+            <div>Products not found</div>
+          )}
+          <SelectInput title="Categories" options={categories} />
+          <SelectInput title="Brands" options={brands} />
+        </div>
+      </Card>
+      <Card title="Products"></Card>
     </div>
   );
 };
