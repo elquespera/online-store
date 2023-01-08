9576c4e25c741ac876f6967ab824be61f59d7799
Vladislav <serov.vladsilaw@yandex.ru>
2023-01-08 15:12:47 +0300

    fix: changed regex and moved const to other file

--- src/components/OrderModal/OrderModal.tsx
+++ src/components/OrderModal/OrderModal.tsx
@@ -1,4 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
+import {
+  ORDER_INPUTS,
+  REDIRECT_INTERVAL,
+  CARD_TYPES,
+} from '../../constants/orderModal';
+import { inputIDs } from '../../types';
 import OrderInput from '../OrderInput/OrderInput';
 import styles from './OrderModal.module.scss';
 
@@ -7,86 +13,6 @@ interface OrderModalProps {
   onCancel: () => void;
   onSuccess: () => void;
 }
-
-const REDIRECT_INTERVAL = 5;
-
-enum inputIDs {
-  name = 0,
-  phone,
-  address,
-  email,
-  cardNumber,
-  cardValid,
-  cardCVV,
-}
-
-const ORDER_INPUTS = [
-  // Personal Details
-  {
-    id: inputIDs.name,
-    value: '',
-    placeholder: 'Name and Surname',
-    hint: 'Name and surname must have 3 characters or more.',
-    pattern: /^.{3,}(\s.{3,})+$/i,
-    valid: true,
-  },
-  {
-    id: inputIDs.phone,
-    value: '',
-    placeholder: 'Phone number',
-    hint: 'At least 9 numbers starting with +',
-    pattern: /^\+[0-9]{9,}$/,
-    valid: true,
-  },
-  {
-    id: inputIDs.address,
-    value: '',
-    placeholder: 'Address',
-    hint: 'Address must have at least 3 words 5 characters or more each.',
-    pattern: /^.{5,}(\s.{5,}){2,}$/i,
-    valid: true,
-  },
-  {
-    id: inputIDs.email,
-    value: '',
-    placeholder: 'Email',
-    hint: 'Provide a valid email address: example@domain.com',
-    pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i,
-    valid: true,
-  },
-
-  //Credit Card Details
-  {
-    id: inputIDs.cardNumber,
-    value: '',
-    placeholder: 'Card Number',
-    hint: 'Must contain 16 numbers',
-    pattern: /^[0-9]{4}(\s[0-9]{4}){3}$/,
-    valid: true,
-  },
-  {
-    id: inputIDs.cardValid,
-    value: '',
-    placeholder: 'Valid Thru',
-    hint: 'MM/YY',
-    pattern: /^[0-9]{2}\/[0-9]{2}$/,
-    valid: true,
-  },
-  {
-    id: inputIDs.cardCVV,
-    value: '',
-    placeholder: 'CVV Code',
-    hint: '3 numbers on the back of the card.',
-    pattern: /^[0-9]{3}$/,
-    valid: true,
-  },
-];
-
-const CARD_TYPES: { [index: string]: string } = {
-  '4': 'visa',
-  '5': 'mastercard',
-  '6': 'discover',
-} as const;
 
 const OrderModal = ({ isOpened, onCancel, onSuccess }: OrderModalProps) => {
   const modal = useRef<HTMLDialogElement>(null);
